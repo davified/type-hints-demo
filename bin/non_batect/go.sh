17ed2d0b4883bd8cb9fd1939f09760e28ce622e7
David Tan <davidtan@thoughtworks.com>
2021-10-10 15:59:19 +1100

    Fix go.sh

--- bin/non_batect/go.sh
+++ bin/non_batect/go.sh
@@ -5,14 +5,10 @@ echo "Installing homebrew if it's not installed..."
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing docker if it's not installed..."
-which docker || brew cask install docker
+brew install  --cask docker
 
 echo "Installing java if it's not installed..."
-which java
-if [ $? -ne 0 ]; then
-  brew tap adoptopenjdk/openjdk
-  brew cask install adoptopenjdk8
-fi
+brew install --cask temurin
 
 # python dependencies, needed for configuring IDE to have proper intellisense and autocomplete
 echo "Installing python3 if it's not installed..."
